1e391a254f5625dea701ec777fd9e3d30bbd6b7b
Okamissu <Okamissu@gmail.com>
2025-08-07 20:42:29 +0200

    Refactor notes API to use MongoDB and add dotenv for environment variable management

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,30 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+const Note = require('./models/note')
+
 app.use(express.json())
 app.use(express.static('dist'))
-
-let notes = [
-  { id: '1', content: 'HTML is easy', important: true },
-  { id: '2', content: 'Browser can execute only JavaScript', important: false },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max([...notes.map((note) => Number(note.id))]) : 0
-  return String(maxId + 1)
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({})
+    .then((notes) => {
+      response.json(notes)
+    })
+    .catch((err) => {
+      console.error('Error fetching notes:', err)
+      response.status(500).send('Internal Server Error')
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -64,7 +59,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
