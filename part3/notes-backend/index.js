0c3651dea1048399551ccac1c8f40d438690c122
Okamissu <Okamissu@gmail.com>
2025-08-08 13:03:47 +0200

    Refactor notes API to handle errors with middleware; add PUT endpoint for updating notes

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -13,21 +13,21 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (request, response, next) => {
   Note.find({})
     .then((notes) => {
       response.json(notes)
     })
-    .catch((err) => {
-      console.error('Error fetching notes:', err)
-      response.status(500).send('Internal Server Error')
-    })
+    .catch((error) => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) response.json(note)
+      else response.status(404).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -45,12 +45,44 @@ app.post('/api/notes', (request, response) => {
   note.save().then((savedNote) => response.json(savedNote))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  // notes = notes.filter((note) => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) return response.status(404).end()
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => response.json(updatedNote))
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'malformatted id' })
+
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
