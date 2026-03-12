ab54c70e7a59ebbfcae5fb130d269b526383348b
Okamissu <okamissu@gmail.com>
2026-03-12 15:26:08 +0100

    refator(part4/bloglist-backend): restructure application into separate modules [ex. 4.2]

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,36 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const { MONGODB_URI, PORT } = process.env
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const app = require('./app')
+const logger = require('./utils/logger')
+const { PORT } = process.env
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port${PORT}`)
 })
