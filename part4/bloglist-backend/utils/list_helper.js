b18da997beb74be5826af787e2831f4124891384
Okamissu <okamissu@gmail.com>
2026-03-13 10:01:25 +0100

    test(part4/bloglist-backend): add tests for totalLikes function [ex 4.4]

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogList) => {
+  return blogList.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
