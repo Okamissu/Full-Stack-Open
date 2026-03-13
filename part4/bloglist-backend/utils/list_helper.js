658faffabc9511dcb06aa9b2aa0d1a9456778af4
Okamissu <okamissu@gmail.com>
2026-03-13 10:22:03 +0100

    test(part4/bloglist-backend): add tests for favoriteBlog function [ex. 4.5]

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -7,7 +7,22 @@ const totalLikes = (blogList) => {
   return blogList.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favoriteBlog = (blogList) => {
+  let currentBest = null
+
+  if (blogList.length < 1) return null
+
+  for (const blog of blogList) {
+    if (!currentBest || blog.likes > currentBest.likes) {
+      currentBest = blog
+    }
+  }
+
+  return currentBest
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
