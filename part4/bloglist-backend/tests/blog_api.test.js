e09d5b4cdadc7f965b07fa3944eea3a1200da032
Okamissu <okamissu@gmail.com>
2026-04-08 17:48:49 +0200

    test(part4/bloglist-backend): refactor & add new tests

--- part4/bloglist-backend/tests/blog_api.test.js
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -14,76 +14,118 @@ beforeEach(async () => {
 })
 
 describe('bloglist api', () => {
-  test('blogs are returned as JSON', async () => {
-    const response = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  describe('when there are initially some blogs saved', () => {
+    test('blogs are returned as JSON', async () => {
+      const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+
+    test('blogs identifier is named "id"', async () => {
+      const response = await api.get('/api/blogs')
+
+      for (let blog of response.body) {
+        assert.ok(blog.id)
+        assert.strictEqual(blog._id, undefined)
+      }
+    })
+
+    test('all blogs are returned', async () => {
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+
+    test('a specific blog is within the returned notes', async () => {
+      const response = await api.get('/api/blogs')
+
+      const contents = response.body.map((r) => r.title)
+      assert(contents.includes('React patterns'))
+    })
   })
 
-  test('blogs identifier is named "id"', async () => {
-    const response = await api.get('/api/blogs')
+  describe('viewing a specific blog resource', () => {
+    test('succeeds with a valid id', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToView = blogsAtStart[0]
 
-    for (let blog of response.body) {
-      assert.ok(blog.id)
-      assert.strictEqual(blog._id, undefined)
-    }
+      const resultNote = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.deepStrictEqual(resultNote.body, blogToView)
+    })
+
+    test('fails with status code 404 if note does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId()
+
+      await api.get(`/api/blogs/${validNonexistingId}`).expect(404)
+    })
+
+    test('fails with status code 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3467'
+
+      await api.get(`/api/blogs/${invalidId}`).expect(400)
+    })
   })
 
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'The Doxie Blog',
-      author: 'Leia',
-      url: 'https://thedoxieblog.blogspot.com/',
-      likes: 0,
-    }
+  describe('addition of a new blog resource', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'The Doxie Blog',
+        author: 'Leia',
+        url: 'https://thedoxieblog.blogspot.com/',
+        likes: 0,
+      }
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/blogs')
-    const blogsAtEnd = response.body
+      const response = await api.get('/api/blogs')
+      const blogsAtEnd = response.body
 
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-    const titles = blogsAtEnd.map((blog) => blog.title)
-    assert(titles.includes('The Doxie Blog'))
-  })
+      const titles = blogsAtEnd.map((blog) => blog.title)
+      assert(titles.includes('The Doxie Blog'))
+    })
 
-  test('adding a new blog without likes defaults it to 0', async () => {
-    const newBlog = {
-      title: 'The Kitty Blog',
-      author: 'Dion',
-      url: 'https://thekittyblog.blogspot.com/',
-    }
+    test('adding a new blog without likes defaults it to 0', async () => {
+      const newBlog = {
+        title: 'The Kitty Blog',
+        author: 'Dion',
+        url: 'https://thekittyblog.blogspot.com/',
+      }
 
-    const response = await api.post('/api/blogs').send(newBlog).expect(201)
+      const response = await api.post('/api/blogs').send(newBlog).expect(201)
 
-    assert.strictEqual(response.body.likes, 0)
-  })
+      assert.strictEqual(response.body.likes, 0)
+    })
 
-  test('adding a new blog with title missing returns 400 bad request', async () => {
-    const newBlog = {
-      author: 'Dion',
-      url: 'https://thekittyblog.blogspot.com/',
-      likes: 0,
-    }
+    test('adding a new blog with title missing returns 400 bad request', async () => {
+      const newBlog = {
+        author: 'Dion',
+        url: 'https://thekittyblog.blogspot.com/',
+        likes: 0,
+      }
 
-    await api.post('/api/blogs').send(newBlog).expect(400)
-  })
-  test('adding a new blog with url missing returns 400 bad request', async () => {
-    const newBlog = {
-      title: 'The Kitty Blog',
-      author: 'Dion',
-      likes: 0,
-    }
+      await api.post('/api/blogs').send(newBlog).expect(400)
+    })
+    test('adding a new blog with url missing returns 400 bad request', async () => {
+      const newBlog = {
+        title: 'The Kitty Blog',
+        author: 'Dion',
+        likes: 0,
+      }
 
-    await api.post('/api/blogs').send(newBlog).expect(400)
+      await api.post('/api/blogs').send(newBlog).expect(400)
+    })
   })
 
   test('deleting a single blog resource', async () => {
@@ -92,17 +134,16 @@ describe('bloglist api', () => {
 
   test('updating the information of a single blog resource', async () => {
     const updatedBlog = {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
+      title: 'Angular patterns',
       author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-      __v: 0,
+      url: 'https://angularpatterns.com/',
+      likes: 0,
     }
-    await api
-      .put('/api/blogs/5a422a851b54a676234d17f7')
-      .send(updatedBlog)
-      .expect(200)
+    const blogsAtStart = await helper.blogsInDb()
+
+    const blogToUpdate = blogsAtStart[0]
+
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200)
   })
 })
 
