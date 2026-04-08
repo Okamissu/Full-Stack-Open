26609f9eecc37887f9fac4db2906338d41c289d4
Okamissu <okamissu@gmail.com>
2026-04-08 17:34:25 +0200

    feat(part4/bloglist-backend): add validation and requirements for blog fields

--- part4/bloglist-backend/models/blog.js
+++ part4/bloglist-backend/models/blog.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  author: { type: String, required: true, minLength: 3 },
+  url: {
+    type: String,
+    required: true,
+    match:
+      /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+  },
   likes: {
     type: Number,
     default: 0,
