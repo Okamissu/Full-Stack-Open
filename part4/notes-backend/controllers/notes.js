3a39d50684bf254a08ac3cefef1a54c6db5a11f3
Okamissu <okamissu@gmail.com>
2026-04-19 16:56:39 +0200

    feat(part4/notes-backend): implement JWT token verification for user authentication

--- part4/notes-backend/controllers/notes.js
+++ part4/notes-backend/controllers/notes.js
@@ -1,6 +1,17 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -25,7 +36,15 @@ notesRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    console.log(decodedToken.id)
 
     if (!user) {
       return response.status(400).json({ error: 'UserId missing or not valid' })
