f2a5ad26c3ebc0c7eab4589fc130647da24e7b85
Okamissu <okamissu@gmail.com>
2026-04-27 18:43:46 +0200

    feat(part4/bloglist-backend): add user creation and retrieval endpoints [ex. 4.15]

--- part4/bloglist-backend/tests/blog_api.test.js
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -1,9 +1,11 @@
 const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
 
 const api = supertest(app)
@@ -144,6 +146,39 @@ describe('bloglist api', () => {
     const blogToUpdate = blogsAtStart[0]
 
     await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200)
+  })
+
+  describe('whem there is initially one user in db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
+
+      const passwordHash = await bcrypt.hash('sekret', 10)
+      const user = new User({ username: 'root', passwordHash })
+
+      await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen',
+      }
+
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+      const usernames = usersAtEnd.map((u) => u.username)
+      assert(usernames.includes(newUser.username))
+    })
   })
 })
 
