91043ab6f2126d2db2a282fa0e1466df0a4aa4be
Okamissu <okamissu@gmail.com>
2026-04-30 08:45:04 +0200

    feat(part4/bloglist-backend): add token validation and author property check in blog creation endpoint [ex. 4.19]

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user')
@@ -15,20 +26,38 @@ blogsRouter.get('/:id', async (request, response) => {
   else response.status(404).end()
 })
 
-blogsRouter.post('/', async (request, response) => {
-  if (
-    !Object.hasOwn(request.body, 'title') ||
-    !Object.hasOwn(request.body, 'url')
-  ) {
-    return response.status(400).send('Missing properties')
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    if (
+      !Object.hasOwn(request.body, 'title') ||
+      !Object.hasOwn(request.body, 'url') ||
+      !Object.hasOwn(request.body, 'author')
+    ) {
+      return response.status(400).json({ error: 'Missing properties' })
+    }
+
+    const token = getTokenFrom(request)
+    if (!token) {
+      return response.status(401).json({ error: 'token missing' })
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    const user = await User.findById(decodedToken.id)
+    if (!user) {
+      return response.status(401).json({ error: 'user not found' })
+    }
+
+    const blog = new Blog({
+      ...request.body,
+      user: user._id,
+    })
+
+    const result = await blog.save()
+    response.status(201).json(result)
+  } catch (error) {
+    next(error)
   }
-
-  const user = await User.findOne({})
-  const blog = new Blog({ ...request.body, user: user._id })
-
-  const result = await blog.save()
-
-  response.status(201).json(result)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
