6dd92248499709d74668ccdd28eaf77fdd33d226
Okamissu <okamissu@gmail.com>
2026-04-30 08:45:14 +0200

    fix(part4/bloglist-backend): improve error handling for JWT token errors

--- part4/bloglist-backend/utils/middleware.js
+++ part4/bloglist-backend/utils/middleware.js
@@ -28,7 +28,13 @@ const errorHandler = (error, request, response, next) => {
     return response
       .status(400)
       .json({ error: 'expected `username` to be unique' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' })
   }
+
+  response.status(500).json({ error: 'internal server error' })
 
   next(error)
 }
