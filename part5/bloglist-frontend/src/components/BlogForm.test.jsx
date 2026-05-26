c1c44e2c003852a74b0ccb84ea613fb8e6ae4f60
Okamissu <okamissu@gmail.com>
2026-05-26 12:39:28 +0200

    test(part5/bloglist-frontend): add tests for BlogForm submission handling

---
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -0,0 +1,29 @@
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BlogForm from './BlogForm'
+
+describe('<BlogForm />', () => {
+  test('Submit handler gets called with right details', async () => {
+    const mockHandler = vi.fn()
+    render(<BlogForm createBlog={mockHandler} />)
+
+    const user = userEvent.setup()
+
+    const titleInput = screen.getByLabelText(/title/i)
+    const authorInput = screen.getByLabelText(/author/i)
+    const urlInput = screen.getByLabelText(/url/i)
+    const createButton = screen.getByRole('button', { name: /create/i })
+
+    await user.type(titleInput, 'BlogTitle')
+    await user.type(authorInput, 'BlogAuthor')
+    await user.type(urlInput, 'https://www.blog.com')
+    await user.click(createButton)
+
+    expect(mockHandler).toHaveBeenCalledTimes(1)
+    expect(mockHandler).toHaveBeenCalledWith({
+      title: 'BlogTitle',
+      author: 'BlogAuthor',
+      url: 'https://www.blog.com',
+    })
+  })
+})
