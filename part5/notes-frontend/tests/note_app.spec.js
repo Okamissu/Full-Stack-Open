cb79460ee820faf6022571e01941b58becf723cf
Okamissu <okamissu@gmail.com>
2026-06-04 11:18:23 +0200

    test(part5/notes-frontend): add note creation test and refactor setup

--- part5/notes-frontend/tests/note_app.spec.js
+++ part5/notes-frontend/tests/note_app.spec.js
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
-
+  })
+  test('front page can be opened', async ({ page }) => {
     const locator = page.getByRole('heading', { name: /notes/i })
     await expect(locator).toBeVisible()
     await expect(
@@ -14,13 +15,28 @@ test.describe('Note app', () => {
   })
 
   test('user can log in', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-
     await page.getByRole('button', { name: /log in/i }).click()
     await page.getByLabel(/username/i).fill('mluukkai')
     await page.getByLabel(/password/i).fill('salainen')
     await page.getByRole('button', { name: /log in/i }).click()
 
     await expect(page.getByText(/Matti Luukkainen logged in/i)).toBeVisible()
+  })
+
+  test.describe('when logged in', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: /log in/i }).click()
+      await page.getByLabel(/username/i).fill('mluukkai')
+      await page.getByLabel(/password/i).fill('salainen')
+      await page.getByRole('button', { name: /log in/i }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: /new note/i }).click()
+      await page.getByLabel(/content/i).fill('a new test note')
+      await page.getByRole('button', { name: /save/i }).click()
+
+      await expect(page.getByText('a new test note')).toBeVisible()
+    })
   })
 })
