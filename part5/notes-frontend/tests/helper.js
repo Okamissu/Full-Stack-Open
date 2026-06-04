80f75630f94571c1bb5e510c1d3d62b970ad87d3
Okamissu <okamissu@gmail.com>
2026-06-04 12:38:34 +0200

    test(part5/notes-frontend): refactor tests to use createNote helper function

--- part5/notes-frontend/tests/helper.js
+++ part5/notes-frontend/tests/helper.js
@@ -5,4 +5,10 @@ const loginWith = async (page, username, password) => {
   await page.getByRole('button', { name: /log in/i }).click()
 }
 
-export { loginWith }
+const createNote = async (page, content) => {
+  await page.getByRole('button', { name: /new note/i }).click()
+  await page.getByLabel(/content/i).fill(content)
+  await page.getByRole('button', { name: /save/i }).click()
+}
+
+export { loginWith, createNote }
