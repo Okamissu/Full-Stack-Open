adf433d38d93b5d4936efdba604e89c87a4ff4d4
Okamissu <okamissu@gmail.com>
2026-06-04 10:40:06 +0200

    test(part5/notes-frontend): add log in test

--- part5/notes-frontend/tests/note_app.spec.js
+++ part5/notes-frontend/tests/note_app.spec.js
@@ -4,12 +4,23 @@ test.describe('Note app', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('http://localhost:5173')
 
-    const locator = page.getByRole('heading', /notes/i)
+    const locator = page.getByRole('heading', { name: /notes/i })
     await expect(locator).toBeVisible()
     await expect(
       page.getByText(
         'Note app, Department of Computer Science, University of Helsinki 2026',
       ),
     ).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: /log in/i }).click()
+    await page.getByRole('textbox').first().fill('mluukkai')
+    await page.getByRole('textbox').last().fill('salainen')
+    await page.getByRole('button', { name: /log in/i }).click()
+
+    await expect(page.getByText(/Matti Luukkainen logged in/i)).toBeVisible()
   })
 })
