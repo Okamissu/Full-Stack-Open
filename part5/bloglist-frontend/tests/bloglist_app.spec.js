c78b9e1796df0281618d98226ed312337fe199dd
Okamissu <okamissu@gmail.com>
2026-06-06 18:09:33 +0200

    test(part5/bloglist-frontend): add login success & fail tests with db & user setup [5.18]

--- part5/bloglist-frontend/tests/bloglist_app.spec.js
+++ part5/bloglist-frontend/tests/bloglist_app.spec.js
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Blog app', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen',
+      },
+    })
+
     await page.goto('/')
   })
 
@@ -14,5 +23,26 @@ test.describe('Blog app', () => {
     await expect(loginForm.getByLabel('Username')).toBeVisible()
     await expect(loginForm.getByLabel('Password')).toBeVisible()
     await expect(loginForm.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+
+  test.describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      const loginForm = page.locator('form')
+
+      await loginForm.getByLabel('Username').fill('mluukkai')
+      await loginForm.getByLabel('Password').fill('salainen')
+      await loginForm.getByRole('button', { name: /login/i }).click()
+
+      await expect(page.getByText(/Matti Luukkainen logged in/i)).toBeVisible()
+    })
+    test('fails with invalid credentials', async ({ page }) => {
+      const loginForm = page.locator('form')
+
+      await loginForm.getByLabel('Username').fill('wrongwrrong')
+      await loginForm.getByLabel('Password').fill('wrongwrrong')
+      await loginForm.getByRole('button', { name: /login/i }).click()
+
+      await expect(page.getByText(/wrong credentials/i)).toBeVisible()
+    })
   })
 })
