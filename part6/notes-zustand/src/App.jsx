9e0b9005f4409650e65284bba03548e5393a59d6
Okamissu <okamissu@gmail.com>
2026-06-23 17:37:22 +0200

    feat(part6/notes-zustand): integrate json-server for note fetching and update store initialization

--- part6/notes-zustand/src/App.jsx
+++ part6/notes-zustand/src/App.jsx
@@ -1,8 +1,17 @@
 import NoteForm from './components/NoteForm'
 import NoteList from './components/NoteList'
 import VisibilityFilter from './components/VisibilityFilter'
+import { useEffect } from 'react'
+import { useNoteActions } from './store'
+import noteService from './services/notes'
 
 const App = () => {
+  const { initialize } = useNoteActions()
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => initialize(notes))
+  }, [initialize])
+
   return (
     <div className="min-h-screen bg-slate-50">
       <div className="max-w-4xl mx-auto p-6 text-slate-900 space-y-4">
--- part6/notes-zustand/src/store.js
+++ part6/notes-zustand/src/store.js
@@ -1,27 +1,10 @@
 import { create } from 'zustand'
 
-const initialNotes = [
-  {
-    id: 1,
-    content: 'Zustand is less complex than Redux',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'React app benefits from custom hooks',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'Remember to sleep well',
-    important: true,
-  },
-]
-
 const useNoteStore = create((set) => ({
-  notes: initialNotes,
+  notes: [],
   filter: 'all',
   actions: {
+    initialize: (notes) => set(() => ({ notes })),
     add: (note) => set((state) => ({ notes: [...state.notes, note] })),
     toggleImportance: (id) =>
       set((state) => ({
