f26ac3fd3609e1016de721234343ae01dc809e12
Okamissu <okamissu@gmail.com>
2026-06-24 08:16:35 +0200

    feat(part6/anecdotes-zustand): integrate json-server for anecdote data and initialize state from fetched data [6.7]

--- part6/anecdotes-zustand/src/App.jsx
+++ part6/anecdotes-zustand/src/App.jsx
@@ -2,8 +2,17 @@ import './index.css'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteFilter from './components/AnecdoteFilter'
+import anecdoteService from './services/anecdotes'
+import { useAnecdoteActions } from './store'
+import { useEffect } from 'react'
 
 const App = () => {
+  const { initialize } = useAnecdoteActions()
+
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => initialize(anecdotes))
+  }, [initialize])
+
   return (
     <main className="max-w-3xl mx-auto p-4 space-y-3">
       <h1>Anecdotes</h1>
