57ca73639d0ecea35c2647cafa00e9925167c07a
Okamissu <okamissu@gmail.com>
2026-06-24 17:18:41 +0200

    test(part6/anecdotes-zustand): verify the component receives a properly filtered list of anecdotes [6.14]

--- part6/anecdotes-zustand/src/components/AnecdoteList.test.jsx
+++ part6/anecdotes-zustand/src/components/AnecdoteList.test.jsx
@@ -1,4 +1,4 @@
-import { render, renderHook, act } from '@testing-library/react'
+import { render, screen, renderHook, act } from '@testing-library/react'
 import AnecdoteList from './AnecdoteList'
 vi.mock('../services/anecdotes', () => ({
   default: {
@@ -11,7 +11,7 @@ vi.mock('../services/anecdotes', () => ({
 
 import anecdoteService from '../services/anecdotes'
 import useAnecdoteStore, { useAnecdoteActions } from '../store'
-import { describe } from 'vitest'
+import { describe, expect } from 'vitest'
 
 describe('<AnecdoteList/>', () => {
   beforeEach(() => {
@@ -45,13 +45,31 @@ describe('<AnecdoteList/>', () => {
       await result.current.initialize()
     })
 
-    const { container } = render(<AnecdoteList />)
+    render(<AnecdoteList />)
 
-    const paragraphs = container.querySelectorAll('p')
-    expect(paragraphs).toHaveLength(3)
+    const paragraphs = screen.queryAllByText(/testing/i)
 
     expect(paragraphs[0]).toHaveTextContent('Testing 2')
     expect(paragraphs[1]).toHaveTextContent('Testing 1')
     expect(paragraphs[2]).toHaveTextContent('Testing 0')
+  })
+
+  test('receives a properly filtered list of anecdotes', async () => {
+    anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
+
+    const { result } = renderHook(() => useAnecdoteActions())
+
+    await act(async () => {
+      await result.current.initialize()
+    })
+
+    useAnecdoteStore.setState({ filter: '1' })
+
+    render(<AnecdoteList />)
+
+    expect(screen.getByText('Testing 1')).toBeInTheDocument()
+
+    expect(screen.queryByText('Testing 0')).not.toBeInTheDocument()
+    expect(screen.queryByText('Testing 2')).not.toBeInTheDocument()
   })
 })
