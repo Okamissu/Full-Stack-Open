0612212030d6f80e0a93acfcaa49915612f6a2dc
Okamissu <okamissu@gmail.com>
2026-06-24 11:01:31 +0200

    style(part6/notes-zustand): adjust layout and button styling

--- part6/notes-zustand/src/components/Note.jsx
+++ part6/notes-zustand/src/components/Note.jsx
@@ -4,14 +4,14 @@ const Note = ({ note }) => {
   const { toggleImportance } = useNoteActions()
 
   return (
-    <li className="flex items-center justify-between py-3 px-3 rounded-sm bg-slate-50 border shadow-sm border-slate-200">
+    <li className="flex gap-2 items-center justify-between py-3 px-3 rounded-sm bg-slate-50 border shadow-sm border-slate-200">
       {note.important ? <strong>{note.content}</strong> : note.content}
 
       <button
         onClick={() => {
           toggleImportance(note.id)
         }}
-        className="bg-slate-200 border border-slate-300 py-1 px-2  shadow-sm rounded-md cursor-pointer transition-colors hover:bg-slate-300"
+        className="bg-slate-200 border border-slate-300 py-1 px-2 whitespace-nowrap shadow-sm rounded-md cursor-pointer transition-colors hover:bg-slate-300"
       >
         {note.important ? 'Set unimportant' : 'Set important'}
       </button>
