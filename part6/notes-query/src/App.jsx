d54d9dc1f39fee4bfec1089fdc8332fb6f145202
Okamissu <okamissu@gmail.com>
2026-06-25 10:56:24 +0200

    feat(part6/notes-query): refactor note handling into custom hook

--- part6/notes-query/src/App.jsx
+++ part6/notes-query/src/App.jsx
@@ -1,50 +1,23 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { createNote, getNotes, updateNote } from './requests'
+import { useNotes } from './hooks/useNotes'
 
 const App = () => {
-  const queryClient = useQueryClient()
-
-  const newNoteMutation = useMutation({
-    mutationFn: createNote,
-    onSuccess: (newNote) => {
-      const notes = queryClient.getQueryData(['notes'])
-      queryClient.setQueryData(['notes'], notes.concat(newNote))
-    },
-  })
-
-  const updateNoteMutation = useMutation({
-    mutationFn: updateNote,
-    onSuccess: (updatedNote) => {
-      const notes = queryClient.getQueryData(['notes'])
-      queryClient.setQueryData(['notes'], () =>
-        notes.map((n) => (n.id === updatedNote.id ? updatedNote : n)),
-      )
-    },
-  })
+  const {
+    notes,
+    isPending,
+    addNote: addNoteToServer,
+    toggleImportance,
+  } = useNotes()
 
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.reset()
-    newNoteMutation.mutate({ content, important: true })
+    addNoteToServer(content)
   }
 
-  const toggleImportance = (note) => {
-    updateNoteMutation.mutate({ ...note, important: !note.important })
-  }
-
-  const result = useQuery({
-    queryKey: ['notes'],
-    queryFn: getNotes,
-  })
-
-  console.log(JSON.parse(JSON.stringify(result)))
-
-  if (result.isPending) {
+  if (isPending) {
     return <div>loading data...</div>
   }
-
-  const notes = result.data
 
   return (
     <div>
