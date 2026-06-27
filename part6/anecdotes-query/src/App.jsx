906874c842b33727a1e55a547efd32ebf1d95b29
Okamissu <okamissu@gmail.com>
2026-06-27 14:12:55 +0200

    feat(part6/anecdotes-query): Implement retrieving anecdotes from the server [6.16]

--- part6/anecdotes-query/src/App.jsx
+++ part6/anecdotes-query/src/App.jsx
@@ -1,18 +1,34 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
 
 const App = () => {
-  const handleVote = (anecdote) => {
+  const handleVote = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3001/anecdotes')
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch notes')
+      }
+
+      return response.json()
     },
-  ]
+    retry: 2,
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isPending) return <p>Loading data...</p>
+
+  if (result.isError)
+    return <div>anecdote service not available due to problems in server</div>
+
+  const anecdotes = result.data
 
   return (
     <div>
