c320141d7bfb8110aa76a264d3c1fae8c5853039
Okamissu <okamissu@gmail.com>
2026-07-05 11:02:01 +0200

    feat(part7/anecdotes-class): add anecdotes data and implement fetching in App component

--- part7/anecdotes-class/src/App.jsx
+++ part7/anecdotes-class/src/App.jsx
@@ -5,10 +5,36 @@ class App extends React.Component {
     super(props)
   }
 
+  state = {
+    anecdotes: [],
+    current: 0,
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/anecdotes')
+      .then((response) => response.json())
+      .then((data) => this.setState({ anecdotes: data }))
+  }
+
+  handleClick = () => {
+    const randId = Math.floor(Math.random() * this.state.anecdotes.length)
+
+    this.setState({ current: randId })
+  }
+
   render() {
+    const { anecdotes, current } = this.state
+
+    if (this.state.anecdotes.length === 0) {
+      return <p>No anecdotes...</p>
+    }
+
     return (
       <div>
-        <h1>anecdote of the day</h1>
+        <h1>Anecdote of the day: #{anecdotes[current].id}</h1>
+        <p>{anecdotes[current].content}</p>
+        <p>Votes: {anecdotes[current].votes}</p>
+        <button onClick={this.handleClick}>Another one</button>
       </div>
     )
   }
